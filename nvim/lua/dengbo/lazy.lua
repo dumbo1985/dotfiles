bedcba9ae76fc3d3a692d965b158e6ccfca70212
dengbo <dumbo1985@126.com>
2024-05-21 12:07:25 +0800

    neovide 12.2 is no longer support backgroud color, remove it.

--- nvim/lua/dengbo/lazy.lua
+++ nvim/lua/dengbo/lazy.lua
@@ -24,14 +24,7 @@ require("lazy").setup({ { import = "dengbo.plugins" }, { import = "dengbo.plugin
 ----- Neovide GUI -----
 vim.o.guifont = "MesloLGS Nerd Font Mono:h12"
 
--- Helper function for transparency formatting
-local alpha = function()
-	return string.format("%x", math.floor((255 * vim.g.transparency) or 0.3))
-end
--- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-
 vim.g.transparency = 0.3
-vim.g.neovide_background_color = "#0f1117" .. alpha()
 
 vim.g.neovide_transparency = 0.3
 vim.g.neovide_window_blurred = true

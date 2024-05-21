f1483b4cbc2de002803705423b1529cca0432455
dengbo <dumbo1985@126.com>
2024-05-21 10:43:29 +0800

    There's no MesloLGS Nerd Font Mono, change it.

--- nvim/lua/dengbo/lazy.lua
+++ nvim/lua/dengbo/lazy.lua
@@ -22,18 +22,18 @@ require("lazy").setup({ { import = "dengbo.plugins" }, { import = "dengbo.plugin
 })
 
 ----- Neovide GUI -----
-vim.o.guifont = "MesloLGS Nerd Font Mono:h12"
+vim.opt.guifont = { "MesloLGS Nerd Font", ":h11" }
 
 -- Helper function for transparency formatting
 local alpha = function()
-	return string.format("%x", math.floor((255 * vim.g.transparency) or 0.3))
+	return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
 end
 -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
 
-vim.g.transparency = 0.3
+vim.g.transparency = 0.8
 vim.g.neovide_background_color = "#0f1117" .. alpha()
 
-vim.g.neovide_transparency = 0.3
+vim.g.neovide_transparency = 0.8
 vim.g.neovide_window_blurred = true
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0

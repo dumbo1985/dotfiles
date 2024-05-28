eb6bed84ad34c90ab48fdf6f9b5d92b9f643cd5f
dengbo <dumbo1985@126.com>
2024-05-28 16:28:48 +0800

    for transparent

--- nvim/lua/dengbo/lazy.lua
+++ nvim/lua/dengbo/lazy.lua
@@ -78,3 +78,5 @@ vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true }
 vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })

314f585283ad52c3b7fb7a012c2714e1a0de50b1
dengbo <dumbo1985@126.com>
2024-05-31 16:29:18 +0800

    change nvim tree settings

--- nvim/lua/dengbo/plugins/nvim-tree.lua
+++ nvim/lua/dengbo/plugins/nvim-tree.lua
@@ -9,13 +9,26 @@ return {
 		vim.g.loaded_netrwPlugin = 1
 
 		nvimtree.setup({
+			sort_by = "case_sensitive",
+			on_attach = on_attach,
+
 			view = {
-				width = 35,
+				width = 40,
 				relativenumber = true,
 				side = "left",
+				signcolumn = "yes",
 			},
+
+			-- project plugin 需要这样设置
+			update_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_cwd = true,
+			},
+
 			-- change folder arrow icons
 			renderer = {
+				group_empty = true,
 				indent_markers = {
 					enable = true,
 				},
@@ -44,19 +57,10 @@ return {
 			git = {
 				ignore = false,
 			},
+
+			system_open = {
+				cmd = "open",
+			},
 		})
-
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-		keymap.set(
-			"n",
-			"<leader>ef",
-			"<cmd>NvimTreeFindFileToggle<CR>",
-			{ desc = "Toggle file explorer on current file" }
-		) -- toggle file explorer on current file
-		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 	end,
 }

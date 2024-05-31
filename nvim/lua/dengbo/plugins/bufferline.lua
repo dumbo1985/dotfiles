4bcf003ae0eb967fc5083a81c65d0a74224acb01
dengbo <dumbo1985@126.com>
2024-05-31 12:22:22 +0800

    change bufferline setting.

--- nvim/lua/dengbo/plugins/bufferline.lua
+++ nvim/lua/dengbo/plugins/bufferline.lua
@@ -1,11 +1,29 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			numbers = "ordinal", --use ordinal
+
+			indicator = {
+				icon = "▎", -- this should be omitted if indicator style is not 'icon'
+				style = "icon",
+			},
+
+			diagnostics = "nvim-lsp", -- Use nvim-lsp
+			-- Make sure nvim-tree can show in the left
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "【自歌自舞自开怀 无拘无束无碍】",
+					highlight = "Directory",
+					text_align = "left",
+				},
+			},
+
+			mode = "tabs",
+			separator_style = "slant",
+		},
+	},
 }
